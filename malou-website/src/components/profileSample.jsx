22dd5a7e3b257b5d86c262fa09cb2209ee1657b1
KugleBlitz007 <136974540+KugleBlitz007@users.noreply.github.com>
2025-04-05 16:47:41 -0400

    Messing arround
    currently learning : https://react.dev/learn/importing-and-exporting-components

---
+++ malou-website/src/components/profileSample.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return (
+        <img 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg/1920px-Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg"
+        alt="Queeeeeteeeeeee"
+        />
+    )
+}
