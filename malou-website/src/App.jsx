4a70af968938133840f39a57b31ede4e626b0a1e
Johann Rajosefa <136974540+KugleBlitz007@users.noreply.github.com>
2025-04-07 16:44:21 -0400

    Learned rendering lists
    - .map
    - .filter
    -<li key={unique ID}>

--- malou-website/src/App.jsx
+++ malou-website/src/App.jsx
@@ -1,37 +1,19 @@
-function Item({ name, isPacked }) {
-  let itemContent = name;
-  if (isPacked) {
-    itemContent = (
-      <del>
-        {name + " ✅"}
-      </del>
-    );
-  }
-  return (
-    <li className="item">
-      {itemContent}
+import { people } from './components/data.jsx';
+import { getImageUrl } from './components/utils.jsx';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
     </li>
   );
-}
-
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item 
-          isPacked={true} 
-          name="Space suit" 
-        />
-        <Item 
-          isPacked={true} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
-  );
+  return <ul>{listItems}</ul>;
 }
--- malou-website/src/components/utils.jsx
+++ malou-website/src/components/utils.jsx
@@ -1,9 +1,8 @@
-export function getImageUrl(person, size = 's') {
+export function getImageUrl(person) {
     return (
       'https://i.imgur.com/' +
       person.imageId +
-      size +
-      '.jpg'
+      's.jpg'
     );
   }
   
