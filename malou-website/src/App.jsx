f52479fdd8ccd612f42930f88832867caa705315
Johann Rajosefa <136974540+KugleBlitz007@users.noreply.github.com>
2025-04-07 14:17:17 -0400

    Learned passing varaiables
    use {variable}
    use {{object}}
    use camelCase for inline CSS in jsx

--- malou-website/src/App.jsx
+++ malou-website/src/App.jsx
@@ -1,11 +1,25 @@
-import Gallery from './components/profileSample.jsx';
-import {Profile} from './components/profileSample.jsx';
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
 
-export default function App() {
+export default function TodoList() {
   return (
-    <div>
-<Gallery />
-<Profile />
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
     </div>
   );
 }
