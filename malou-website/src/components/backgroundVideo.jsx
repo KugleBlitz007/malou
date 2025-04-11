7319bbd9a29376d636a94cfecf8a76c7bc31e655
KugleBlitz007 <136974540+KugleBlitz007@users.noreply.github.com>
2025-04-10 21:06:49 -0400

    Website fixed, need to implement proper dropdown

--- malou-website/src/components/backgroundVideo.jsx
+++ malou-website/src/components/backgroundVideo.jsx
@@ -5,7 +5,7 @@ const backgroundVideo = () => {
   return (
     <div>
         {/*<div className='absolute w-full h-full top-0 left-0 bg-black/50'></div>*/}
-        <video className="w-full bg-cover object-contain" src={videoBack} autoPlay loop muted />
+        <video className="w-full bg-cover object-contain" src={videoBack} autoPlay loop muted playsInline/>
     </div>
   )
 }
